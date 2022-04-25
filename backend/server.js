69a05a5fafe251757895bd9652ef13f6afc3ae15
Karan Mann <62376840+karanmann@users.noreply.github.com>
2022-04-25 10:57:27 +0200

    Backend-Routes-Error Handling
    Co-authored-by: GolnooshMasoudi <GolnooshMasoudi@users.noreply.github.com>
    Co-authored-by: SaniaNaz <sanianaz@users.noreply.github.com>
    Co-authored-by: Valeria <avreally@users.noreply.github.com>

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const data = require('./data/data.json') 
+let error1 = ""
+
+const PORT = 8080;
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Server is Running!");
+});
+
+app.get('/data', (req, res) => {
+  try { 
+    res.json(data)
+  } catch (error) {
+    error1 = "Some Error"
+  }
+  console.log(error1)
+})
+
+app.get('/error', (req, res) => {
+  res.send(error1)
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
