0e79c4a694d1f4c0f4f44532089fbf7106da7fb2
Karan Mann <karan.mann@outlook.com>
2022-04-27 10:58:37 +0200

    Navbar Styling updated

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,29 +1,47 @@
-import React from 'react';
-import '../styles/Navbar.css';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import Badge from '@mui/material/Badge';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "../styles/Navbar.css";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import Badge from "@mui/material/Badge";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="container">
-      <div className="navbar-wrapper">
-        <div className="left">
-        <div className="logo">FLORgeous</div>
-        </div>
-        <div className="right">
-          <Link className="animated-links" to="/">Home</Link>
-          <Link className="animated-links" to="/product">Products</Link>
-          <Link className="animated-links" to="/register">Register</Link>
-          <Link className="animated-links" to="/signin">SignIn</Link>
-          <Link className="animated-links" to="/cart">Cart</Link>
-          <div className="menu-item">
-            <Badge badgeContent={4} color="primary">
-              <ShoppingCartIcon />
-            </Badge>
-          </div>
-        </div>
+    <div className="navbar">
+      <div>
+        <h1 className="navbar-title">FLORgeous</h1>
       </div>
+      <nav>
+        <ul className="navbar-links-container list">
+          <li className="items">
+            <Link className="animated-links" to="/">
+              Home
+            </Link>
+          </li>
+          <li className="items">
+            <Link className="animated-links" to="/product">
+              Products
+            </Link>
+          </li>
+          <li className="items">
+            <Link className="animated-links" to="/register">
+              Register
+            </Link>
+          </li>
+          <li className="items">
+            <Link className="animated-links" to="/signin">
+              SignIn
+            </Link>
+          </li>
+          <li className="items">
+            <Link className="animated-links" to="/cart">
+              Cart
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <Badge badgeContent={4} color="primary">
+        <ShoppingCartIcon />
+      </Badge>
     </div>
   );
 };
